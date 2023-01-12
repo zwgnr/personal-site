ea0dabbc084993a816f69d0e25236235a1241552
zwagnr <zachwagner@hey.com>
2023-01-12 14:19:04 -0500

    3rd times a charm

--- dist/ThemeSwitcher.js
+++
@@ -1,7 +0,0 @@
-const handleThemeSwitch = () => {
-  let setTheme;
-  let currentTheme =
-    document.documentElement.attributes.getNamedItem("data-theme")?.value; // 'light' | 'dark'
-  currentTheme === "light" ? (setTheme = "dark") : (setTheme = "light");
-  document.dispatchEvent(new CustomEvent("set-theme", { detail: setTheme })); // or pass `null` to clear the saved setting}
-};
