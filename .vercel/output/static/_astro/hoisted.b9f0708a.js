7c93fcbf65c629da04ae9202cfafbbd4ef07f4b5
zwagnr <zachwagner@hey.com>
2023-03-29 13:00:21 -0400

    finish about, projects, and fix font cls issue

---
+++ .vercel/output/static/_astro/hoisted.b9f0708a.js
@@ -0,0 +1,1 @@
+import"./Nav.astro_astro_type_script_index_0_lang.17e11def.js";
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server'
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
+  output: 'server',
+  adapter: vercel()
 });
