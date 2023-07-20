e7f2665d97048bbf19ee24f50a160513c3471fc7
zwgnr <86675944+zwgnr@users.noreply.github.com>
2023-07-20 15:24:32 -0400

    bump deps and fix dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,10 @@
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
-  darkMode: "class",
-  plugins: [require('@tailwindcss/forms'),],
+  darkMode: ["class", '[data-theme="dark"]'],
+  plugins: [require("@tailwindcss/forms")],
   theme: {
     extend: {
       fontFamily: {
