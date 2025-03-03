3192bb00e825354d83b5e89e92bab78c0f40d8a4
Zach <86675944+zwgnr@users.noreply.github.com>
2025-03-02 21:39:47 -0500

    upgrade astro (#2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
+import icon from "astro-icon";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  integrations: [tailwind(), icon()],
   output: 'server',
   adapter: vercel()
 });
